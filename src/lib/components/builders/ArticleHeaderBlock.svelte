dee49720534276a26b51b6d37df8b969ccc3a77d
FrancoisDF <delpechdefrayssinet.francois@gmail.com>
2026-01-11 18:38:32 +0100

    fix: change the blog design

--- src/lib/components/builders/ArticleHeaderBlock.svelte
+++ src/lib/components/builders/ArticleHeaderBlock.svelte
@@ -41,7 +41,7 @@
 	
 	<!-- Article Header -->
 	<div class="mx-auto max-w-4xl px-4 pb-12 sm:px-6 lg:px-8">
-		<div class="rounded-xl bg-white p-8 shadow-lg md:p-12">
+		<div>
 			<!-- Featured Image -->
 			{#if featuredImage}
 				<div class="mx-auto max-w-4xl overflow-hidden rounded-xl">
